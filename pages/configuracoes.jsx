45e587ed69fde36aaf1879afdb20052a1a563a89
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-12-24 22:41:16 -0300

    atualizaçao-dashboard

--- pages/configuracoes.jsx
+++ pages/configuracoes.jsx
@@ -1,12 +1,25 @@
 import Heading from "src/UI/Heading/Heading";
-import GridChart from "src/components/GridChart/GridChart";
 
-export default function Sales() {
+export default function Configuration() {
   return (
     <section>
-      <Heading title={'Configurações'} subtitle={'Aqui está o seu histórico de vendas'} />
+      <Heading title={'Configurações'} subtitle={'Defina as Configurações da aplicação'} />
+      <form>
 
-      <GridChart />
+        <div>
+          <label htmlFor="name" className="hidden">Nome da empresa</label>
+          <input type="text" name="name" id="name" placeholder="Nome da empresa" />
+        </div>
+
+        <div>
+          <label htmlFor="price" className="hidden">Meta de vendas</label>
+          <input type="number" name="price" id="price" placeholder="Meta de vendas" />
+        </div>
+
+        <button className="btn btn-primary">
+          Salvar
+        </button>
+      </form>
     </section>
   )
 }
